f51606a2419be152234cb88cf3ad7258b1edb15f
Eloisa-R <rios.rico.eloisa@gmail.com>
2021-02-06 16:54:09 +0100

    First commit

---
+++ tracks-co2-analytics/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-start">
+     <router-link class="navbar-item" to="/">Home</router-link> |
+     <router-link class="navbar-item" to="/analytics">CO2 Analytics</router-link>
+    </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ tracks-co2-analytics/src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">
+    Placeholder for Home Page
+  </div>
+</template>
